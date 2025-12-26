73e0e7efbbfc9930ca20b3e06f3f869f14f84a5d
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-12-26 21:47:03 +0500

    question 2  student information portal

--- exam/server/server.js
+++ exam/server/server.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const expressLayouts = require("express-ejs-layouts");
+
 
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
 
+app.use(expressLayouts);
+app.set("layout", "base"); 
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.get("/", (req, res) => {
-    res.send("Backend is running");
-});
+
+const studentRoutes = require("./routes/studentRoutes");
+app.use("/", studentRoutes);
 
 
 const PORT = 5000;
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
