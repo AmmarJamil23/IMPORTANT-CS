9c9f256af22b248141d92d25d90d673c81f33ad4
AmmarJamil23 <ammarjamil7000@gmail.com>
2026-01-27 23:18:08 +0500

    updated matrix in the backend next is api call

--- transformer-lab/server/index.js
+++ transformer-lab/server/index.js
@@ -14,9 +14,35 @@ app.use(express.json());
 //Test route
 app.get("/api/health", (req, res) => {
     res.json({
-        status: "Server is running",
-        message: "Transformer Lab Backend"
+        status: "Server is running"
     });
+});
+
+//route of calculating attention
+app.post('/api/calculate-attention', (req, res) => {
+    const { tokens } = req.body;
+
+    if (!tokens || !Array.isArray(tokens)) {
+        return res.status(400).json({ error: 'Invalid tokens' });
+    }
+
+    const seqLen = tokens.length;
+
+    const matrix = [];
+    for (let i = 0; i< seqLen; i++) {
+        const row = [];
+        let sum = 0;
+
+        for (let j = 0; j< seqLen; j++) {
+            const value = Math.random();
+            row.push(value);
+            sum += value;
+        }
+
+        const normalized = row.map(val => val / sum);
+        matrix.push(normalized);
+    }
+    res.json({ attentionMatrix: matrix });
 });
 
 //start server
